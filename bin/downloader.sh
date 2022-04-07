091ae20642a4b0c35b179ca2acfcc67b02cb3b71
nxjosephofficial <61632416+nxjosephofficial@users.noreply.github.com>
2022-04-07 13:15:02 +0300

    Add files via upload

---
+++ bin/downloader.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while true; do
+echo
+echo "mp3, mp4 or webm?"
+echo "q for quit."
+echo
+
+read type
+
+if [[ "$type" == "mp3" ]]; then
+    echo "Enter your link please:"
+    read link
+    yt-dlp -x --audio-format mp3 $link
+elif [[ $type == "mp4" ]]; then
+    echo "Enter your link please:"
+    read link
+    yt-dlp -f 22 $link
+
+elif [[ $type == "webm" ]]; then
+    echo "Enter your link please:"
+    read link
+    yt-dlp $link
+
+elif [[ $type == "q" ]]; then
+    break
+
+else
+    echo "Oops, something went wrong. :("
+fi
+done
