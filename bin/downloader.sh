b5ab6356624673f10af721ab46b5fcdcf58280f5
nxjosephofficial <61632416+nxjosephofficial@users.noreply.github.com>
2022-04-13 01:06:03 +0300

    Update downloader.sh

--- bin/downloader.sh
+++ bin/downloader.sh
@@ -16,15 +16,12 @@ elif [[ $type == "mp4" ]]; then
     echo "Enter your link please:"
     read link
     yt-dlp -f 22 $link
-
 elif [[ $type == "webm" ]]; then
     echo "Enter your link please:"
     read link
     yt-dlp $link
-
 elif [[ $type == "q" ]]; then
     break
-
 else
     echo "Oops, something went wrong. :("
 fi
