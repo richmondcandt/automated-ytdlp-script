95ff095a5b86c991f74423b1a30d4e460f9c459f
nxjosephofficial <61632416+nxjosephofficial@users.noreply.github.com>
2022-05-01 06:07:58 +0300

    Update optional.sh
    I added new feature to optional.sh
    Now it asks for name to rename downloader.sh, So you can run downloader.sh with any name you want and everywhere!

--- optional/optional.sh
+++ optional/optional.sh
@@ -4,11 +4,13 @@ echo "Do you want to continue? Y/N"
 
 read answer
 
-if [[ $answer == "Y" || $answer == "y" ]]; then
+if [[ "$answer" == "Y" || "$answer" == "y" ]]; then
 echo "Okay, script working."
-sudo cp ../bin/*.sh /usr/local/bin/
+echo "What name do you want to call it? it's called downloader.sh by default."
+read name
+sudo cp ../bin/downloader.sh /usr/local/bin/"$name"
 echo "OK!"
-elif [[ $answer == "N" || $answer == "n" ]]; then
+elif [[ "$answer" == "N" || "$answer" == "n" ]]; then
 echo "Okay, script will not work."
 else
 echo "Oops, something went wrong. :("
