233d00542f6353a2a8bcf7faedf653b349fd06dd
nxjosephofficial <61632416+nxjosephofficial@users.noreply.github.com>
2022-05-01 05:40:30 +0300

    Update downloader.sh
    I put variables in quotes.

--- bin/downloader.sh
+++ bin/downloader.sh
@@ -11,16 +11,16 @@ read type
 if [[ "$type" == "mp3" ]]; then
     echo "Enter your link please:"
     read link
-    yt-dlp -x --audio-format mp3 $link
-elif [[ $type == "mp4" ]]; then
+    yt-dlp -x --audio-format mp3 "$link"
+elif [[ "$type" == "mp4" ]]; then
     echo "Enter your link please:"
     read link
-    yt-dlp -f 22 $link
-elif [[ $type == "webm" ]]; then
+    yt-dlp -f 22 "$link"
+elif [[ "$type" == "webm" ]]; then
     echo "Enter your link please:"
     read link
-    yt-dlp $link
-elif [[ $type == "q" ]]; then
+    yt-dlp "$link"
+elif [[ "$type" == "q" ]]; then
     break
 else
     echo "Oops, something went wrong. :("
