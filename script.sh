a0db474f5391e8bf7c5489f44af4b7a701f4809f
nxjosephofficial <nxjosephofficial@protonmail.com>
2022-10-30 18:37:20 +0300

    Added channel download feature

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ PURPLE='\033[0;35m' # Purple
 LBLUE='\033[0;34m' # Light Blue
 whereami=$(pwd) # for support '.' in path input
 while true; do
-printf "${CYAN}mp3, mp4, webm,${YELLOW} q, exit${NC}\n"
+printf "${CYAN}mp3, mp4, webm, channel,${YELLOW} q, exit${NC}\n"
 read type
 if [ "$type" == "mp3" ]; then
 printf "${PURPLE}Link: ${NC}"
@@ -41,6 +41,30 @@ yt-dlp "$link" -o "$path""%(title).200s.%(ext)s"
 fi
 elif [[ "$type" == "q" || "$type" == "exit" ]]; then
 break
+elif [ "$type" == "channel" ]; then
+printf "Enter Channel ID\n"
+read channel
+if [ "$channel" == "" ]; then
+    printf "${RED}Channel can not be empty!${NC}\n"
+    break
+fi
+printf "Select video formats?\n1) 720p, 2) 1080p, 3) best, 4) mp3\n"
+read format
+if [ "$format" != "" ]; then
+    if [[ "$format" == "1" || "$format" == "720p" ]]; then
+        yt-dlp -f 'bv*[height=720]+ba' --embed-thumbnail --embed-metadata --download-archive $channel.txt https://www.youtube.com/channel/$channel/videos -o '%(channel)s/%(title).200s.%(ext)s'
+    elif [[ "$format" == "2" || "$format" == "1080p" ]]; then
+        yt-dlp -f 'bv*[height=1080]+ba' --embed-thumbnail --embed-metadata --download-archive $channel.txt https://www.youtube.com/channel/$channel/videos -o '%(channel)s/%(title).200s.%(ext)s'
+    elif [[ "$format" == "3" || "$format" == "best" ]]; then
+        yt-dlp -f b --embed-thumbnail --embed-metadata --download-archive $channel.txt https://www.youtube.com/channel/$channel/videos -o '%(channel)s/%(title).200s.%(ext)s'
+    elif [[ "$format" == "4" || "$format" == "mp3" ]]; then
+        yt-dlp -x --audio-format mp3 --embed-thumbnail --embed-metadata --download-archive $channel.txt https://www.youtube.com/channel/$channel/videos -o '%(channel)s/%(title).200s.%(ext)s'    
+    else
+        printf "${RED}Wrong format!${NC}\n"
+    fi
+else
+printf "${RED}Format can not be empty!${NC}\n"
+fi
 else
 printf "${RED}Oops, something went wrong. :(${NC}\n"
 break
